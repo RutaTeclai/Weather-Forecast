4a67b284cbee57122c991e398aa9338768b6cb38
RutaTeclai <rutisyear@gmail.com>
2020-11-11 22:17:24 +0000

    Define route('/forecast') implement function show_forecast_page() if a user successfully logged in

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 """Server for weather forecasts app."""
 
 from flask import (Flask, render_template, request,
-                    flash, session, redirect)
+                    flash, session, redirect, requests)
 from model import connect_to_db
 import crud
 from jinja2 import StrictUndefined
@@ -31,7 +31,6 @@ def create_user_account():
     fname= request.form.get('fname')
     lname= request.form.get('lname')
     email= request.form.get('email')
-    print(email)
     password= request.form.get('password')
     city= request.form.get('city')
     state= request.form.get('state')
@@ -40,8 +39,8 @@ def create_user_account():
     print(user)
 
     if user:
-        print('entering')
         flash("The email already in use. Use different email!")
+        
 
     else:
         crud.create_user(fname, lname, email, password, city, state)
@@ -50,6 +49,38 @@ def create_user_account():
     return redirect('/')
 
 
+@app.route('/forecast')
+def show_forecast_page():
+    email= request.args.get('email')
+    password= request.args.get('password')
+
+    user= crud.get_user_by_email(email)
+    if user:
+
+        return render_template('forecastpage.html')
+    else:
+        flash("Enter correct email and password or create a new user account")
+        return redirect('/')
+
+
+def city_geodata(city_name,state):
+    coordinates = []
+    city_data = open('data/trial.json').read()
+
+    city_data_dict = json.loads(city_data)
+    for city in city_data_dict:
+        
+        if city['city'] == city_name and city['state_id'] == state:
+            latitude = city['lat']
+            longitude = city['lng']
+            coordinates.extend([latitude,longitude])
+            
+    return (coordinates)
+
+def get_forecast_office(coordinate_list):
+    lat, lng = coordinate_list
+    res = requests.get('https://api.weather.gov/points/44.7544,-93.3631')
+    res = requsests.get(f'https://api.weather.gov/points/{lat},{lng}')
 
 
 if __name__ == '__main__':
